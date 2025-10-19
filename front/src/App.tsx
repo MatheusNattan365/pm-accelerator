1c02668c7c6220538bc72a19a61747d3faa6e7da
Matheus Nattan <msantos-ext@techforti.com.br>
2025-10-19 18:04:44 -0300

    Refactor App component by removing unused imports and streamlining routing setup

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,5 @@
-import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { WeatherData, SavedRecord, searchWeather, listRecords, LocationType } from './api';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import SearchForm from './components/SearchForm';
-import WeatherCard from './components/WeatherCard';
-import ForecastList from './components/ForecastList';
-import SavedList from './components/SavedList';
-import InfoButton from './components/InfoButton';
-import LinkedInButton from './components/LinkedInButton';
-import PersonalLinkedInButton from './components/PersonalLinkedInButton';
-import Toast from './components/Toast';
-import LoadingBackdrop from './components/LoadingBackdrop';
 
 import WeatherApp from './components/WeatherApp';
 
